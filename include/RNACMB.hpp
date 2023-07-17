7edf297c569b3c48ce7aea23c93fd15ef3e6bb72
iriveros <56614758+iriveros@users.noreply.github.com>
2023-07-17 14:28:45 -0400

    Update RNACMB.hpp

--- include/RNACMB.hpp
+++ include/RNACMB.hpp
@@ -44,7 +44,7 @@ constexpr float RADIUS_O   = 0.75;
 constexpr float INTERACTION_DISTANCE = 3.6;
 constexpr float DEFAULT_RMSD_LIMIT = 0.5;
 constexpr float DEFAULT_WC_RMSD_LIMIT = 2.5;
-constexpr char  DEFAULT_OUTPUT_FILENAME[] = "RNACMB_Output.dat";
+constexpr char  DEFAULT_OUTPUT_FILENAME[] = "TAPERSS_Output.dat";
 constexpr bool STRUCTURE_BUILD_LIMIT = false;
 constexpr bool PERFORM_CHECKS_ON_CUSTOM_BUILD = true;
 constexpr uint8_t MATRIX_DIMENSION2 = 3; //DIM2 = 3, for X,Y,Z
